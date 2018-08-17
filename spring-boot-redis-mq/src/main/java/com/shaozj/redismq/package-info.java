2d2b963b4082680bb4f4cc5f366c92899796350d
shaozj <409132333@qq.com>
2018-08-17 10:08:12 +0800

    1111111111

--- spring-boot-redis-mq/src/main/java/com/shaozj/redismq/package-info.java
+++ spring-boot-redis-mq/src/main/java/com/shaozj/redismq/package-info.java
@@ -6,7 +6,7 @@
  * 1.RabbitMQ：相对重量级高并发的情况，比如数据的异步处理、任务的串行执行等。
  * 2.Kafka：基于pull的模式来处理，具体很高的吞吐量，一般用来进行日志存储和收集
  * 3.Redis：轻量级高并发，实时性要求高的情况，比如缓存、秒杀，及时的数据分析（elk日志分析框架，使用的就是redis）
- * @author Administrator
+ * @author Administrator 
  *
  */
 package com.shaozj.redismq;
